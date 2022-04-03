f20fc86eb0dd35fbffe18e28c1ae11941cc482e4
Oganesman <96392987+qweRTyiOp123789@users.noreply.github.com>
2022-04-03 14:43:02 +0300

    Add files via upload

--- forms.component.ts
+++ forms.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class FormsComponent {
 	@Input() config: any = {};
 	@Input() doc: any = {};
-	@Output() sign: EventEmitter<any> = new EventEmitter();
+	@Output("submit") sign: EventEmitter<any> = new EventEmitter();
 	constructor(
 	) { }
 }
