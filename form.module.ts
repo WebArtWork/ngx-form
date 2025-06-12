b7a8bff86f947c577f1e1a5c52523c581e9aa55b
Nika <danzi6386@gmail.com>
2025-06-12 18:39:09 +0300

    bring back standaloneless code

--- form.module.ts
+++ form.module.ts
@@ -5,10 +5,10 @@ import { ModalFormComponent } from './modals/modal-form/modal-form.component';
 import { ModalFormButtonComponent } from './modals/modal-form/modal-form-button/modal-form-button.component';
 import { ModalUniqueComponent } from './modals/modal-unique/modal-unique.component';
 import { CommonModule } from '@angular/common';
-
+import { ButtonModule } from '../button/button.module';
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, ButtonModule],
 	declarations: [
 		FormComponent,
 		FormComponentComponent,
